bca4a56f954f136f5596985fa382beafb1ac4784
Shawn Nock <nock@nocko.org>
2012-08-07 00:06:12 -0400

    Laying out v2 more intelligently... wip

--- board.cmd
+++ board.cmd
@@ -1,5 +1,13 @@
 # Pin name action command file
 
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
 # Start of element J1
 ChangePinName(J1, 3, 3)
 ChangePinName(J1, 2, 2)
@@ -65,18 +73,14 @@ ChangePinName(R5, 2, 2)
 ChangePinName(R1, 1, 1)
 ChangePinName(R1, 2, 2)
 
-# Start of element R3
-ChangePinName(R3, 1, 1)
-ChangePinName(R3, 2, 2)
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
 
 # Start of element U2
 ChangePinName(U2, 1, Iout)
 ChangePinName(U2, 2, Sense+)
 ChangePinName(U2, 3, Sense-)
-
-# Start of element R4
-ChangePinName(R4, 1, 1)
-ChangePinName(R4, 2, 2)
 
 # Start of element U4
 ChangePinName(U4, 8, A1)
@@ -144,15 +148,7 @@ ChangePinName(U6, 6, Vcc)
 ChangePinName(U6, 7, GND)
 ChangePinName(U6, 8, SHDN)
 
-# Start of element R2
-ChangePinName(R2, 1, 1)
-ChangePinName(R2, 2, 2)
-
 # Start of element U1
 ChangePinName(U1, 1, V+)
 ChangePinName(U1, 2, R)
 ChangePinName(U1, 3, V-)
-
-# Start of element C1
-ChangePinName(C1, 1, 1)
-ChangePinName(C1, 2, 2)
