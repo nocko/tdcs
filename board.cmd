1ebb29c4439ec7394cdb78030022070aa2959112
Shawn Nock <nock@nocko.org>
2012-08-07 23:41:20 -0400

    initial commit of v3, USB w/o uController

--- board.cmd
+++ board.cmd
@@ -1,108 +1,30 @@
 # Pin name action command file
 
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 3, 3)
+ChangePinName(R1, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
 # Start of element R2
 ChangePinName(R2, 1, 1)
 ChangePinName(R2, 2, 2)
-
-# Start of element R3
-ChangePinName(R3, 1, 1)
-ChangePinName(R3, 2, 2)
 
 # Start of element J1
 ChangePinName(J1, 3, 3)
 ChangePinName(J1, 2, 2)
 ChangePinName(J1, 1, 1)
 
-# Start of element C5
-ChangePinName(C5, 2, 2)
-ChangePinName(C5, 1, 1)
-
-# Start of element U3
-ChangePinName(U3, 1, TEST/SBWTCK)
-ChangePinName(U3, 2, DVcc)
-ChangePinName(U3, 3, P2.5/Rosc/CA5)
-ChangePinName(U3, 4, DVss)
-ChangePinName(U3, 5, XOUT/P2.7/CA7)
-ChangePinName(U3, 6, XIN/P2.6/CA6)
-ChangePinName(U3, 7, RST/NMI/SBWTDIO)
-ChangePinName(U3, 8, P2.0/ACLK/A0/CA2)
-ChangePinName(U3, 9, P2.1/TAINCLK/SMCLK/A1/CA3)
-ChangePinName(U3, 10, P2.2/TA0.0/A2/CA4/CAOUT)
-ChangePinName(U3, 11, P3.0/UCB0STE/UCA0CLK/A5)
-ChangePinName(U3, 12, P3.1/UCB0SIMO/UCB0SDA)
-ChangePinName(U3, 13, P3.2/UCB0SOMI/UCB0SCL)
-ChangePinName(U3, 14, P3.3/UCB0CLK/UCA0STE)
-ChangePinName(U3, 15, P3.4/UCA0TXD/UCA0SIMO)
-ChangePinName(U3, 16, P3.5/UCA0RXD/UCA0SOMI)
-ChangePinName(U3, 17, P3.6/TA1.0/A6)
-ChangePinName(U3, 18, P3.7/TA1.1/A7)
-ChangePinName(U3, 19, P2.3/TA0.1/A3/VREF-/VeREF-/CA0)
-ChangePinName(U3, 20, P2.4/TA0.2/A4/VREF+/VeREF+/CA1)
-ChangePinName(U3, 21, P1.0/TACLK/ADC10CLK/CAOUT)
-ChangePinName(U3, 22, P1.1/TA0.0/TA1.0)
-ChangePinName(U3, 23, P1.2/TA0.1)
-ChangePinName(U3, 24, P1.3/TA0.2)
-ChangePinName(U3, 25, P1.4/SMCLK/TCK)
-ChangePinName(U3, 26, P1.5/TA0.0/TMS)
-ChangePinName(U3, 27, P1.6/TA0.1/TDI/TCLK)
-ChangePinName(U3, 28, P1.7/TA0.2/TDO/TDI)
-
-# Start of element R7
-ChangePinName(R7, 1, 1)
-ChangePinName(R7, 2, 2)
-
-# Start of element D1
-ChangePinName(D1, 2, 2)
-ChangePinName(D1, 1, 1)
-
-# Start of element R8
-ChangePinName(R8, 1, 1)
-ChangePinName(R8, 2, 2)
-
-# Start of element S1
-
-# Start of element R6
-ChangePinName(R6, 1, 1)
-ChangePinName(R6, 2, 2)
-
-# Start of element R5
-ChangePinName(R5, 1, 1)
-ChangePinName(R5, 2, 2)
-
-# Start of element R1
-ChangePinName(R1, 1, 1)
-ChangePinName(R1, 2, 2)
-
-# Start of element R4
-ChangePinName(R4, 1, 1)
-ChangePinName(R4, 2, 2)
-
-# Start of element U2
-ChangePinName(U2, 1, Iout)
-ChangePinName(U2, 2, Sense+)
-ChangePinName(U2, 3, Sense-)
-
-# Start of element U4
-ChangePinName(U4, 8, A1)
-ChangePinName(U4, 7, W1)
-ChangePinName(U4, 6, Vdd)
-ChangePinName(U4, 5, CLK)
-ChangePinName(U4, 4, SDI)
-ChangePinName(U4, 3, CS)
-ChangePinName(U4, 2, GND)
-ChangePinName(U4, 1, B1)
-
 # Start of element C2
 ChangePinName(C2, 2, 2)
 ChangePinName(C2, 1, 1)
 
-# Start of element C3
-ChangePinName(C3, 2, 2)
-ChangePinName(C3, 1, 1)
-
-# Start of element C4
-ChangePinName(C4, 2, 2)
-ChangePinName(C4, 1, 1)
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
 
 # Start of element J2
 ChangePinName(J2, 5, GND)
@@ -110,43 +32,15 @@ ChangePinName(J2, 3, D+)
 ChangePinName(J2, 2, D-)
 ChangePinName(J2, 1, Vbus)
 
-# Start of element U5
-ChangePinName(U5, 25, AGND)
-ChangePinName(U5, 7, GND)
-ChangePinName(U5, 18, GND)
-ChangePinName(U5, 21, GND)
-ChangePinName(U5, 26, TEST)
-ChangePinName(U5, 12, CBUS4)
-ChangePinName(U5, 14, CBUS3)
-ChangePinName(U5, 13, CBUS2)
-ChangePinName(U5, 22, CBUS1)
-ChangePinName(U5, 23, CBUS0)
-ChangePinName(U5, 6, RI#)
-ChangePinName(U5, 10, DCD#)
-ChangePinName(U5, 27, OSCI)
-ChangePinName(U5, 28, OSCO)
-ChangePinName(U5, 17, 3V3OUT)
-ChangePinName(U5, 4, VccIO)
-ChangePinName(U5, 20, Vcc)
-ChangePinName(U5, 16, USBDM)
-ChangePinName(U5, 15, USBDP)
-ChangePinName(U5, 19, RESET#)
-ChangePinName(U5, 9, DSR#)
-ChangePinName(U5, 2, DTR#)
-ChangePinName(U5, 11, CTS#)
-ChangePinName(U5, 3, RTS#)
-ChangePinName(U5, 5, RXD)
-ChangePinName(U5, 1, TXD)
-
-# Start of element U6
-ChangePinName(U6, 4, C2+)
-ChangePinName(U6, 3, C2-)
-ChangePinName(U6, 2, C1+)
-ChangePinName(U6, 1, C1-)
-ChangePinName(U6, 5, Vout)
-ChangePinName(U6, 6, Vcc)
-ChangePinName(U6, 7, GND)
-ChangePinName(U6, 8, SHDN)
+# Start of element U2
+ChangePinName(U2, 4, C2+)
+ChangePinName(U2, 3, C2-)
+ChangePinName(U2, 2, C1+)
+ChangePinName(U2, 1, C1-)
+ChangePinName(U2, 5, Vout)
+ChangePinName(U2, 6, Vcc)
+ChangePinName(U2, 7, GND)
+ChangePinName(U2, 8, SHDN)
 
 # Start of element U1
 ChangePinName(U1, 1, V+)
