2118a70a35448e06d29dedca2b661273cf729481
Shawn Nock <nock@nocko.org>
2012-09-03 21:03:40 -0400

    Initial commit of v4 design

--- board.cmd
+++ board.cmd
@@ -1,50 +1,61 @@
 # Pin name action command file
 
+# Start of element S1
+ChangePinName(S1, 1, 1)
+ChangePinName(S1, 3, 3)
+ChangePinName(S1, 4, 4)
+ChangePinName(S1, 2, 2)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element B1
+ChangePinName(B1, 2, -)
+ChangePinName(B1, 1, +)
+
 # Start of element C4
-ChangePinName(C4, 2, -)
-ChangePinName(C4, 1, +)
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element CONN1
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element U2
+ChangePinName(U2, 1, C1-)
+ChangePinName(U2, 2, C2+)
+ChangePinName(U2, 3, C2-)
+ChangePinName(U2, 4, -Vout)
+ChangePinName(U2, 5, GND)
+ChangePinName(U2, 6, Vin)
+ChangePinName(U2, 7, C1+)
+ChangePinName(U2, 8, +Vout)
 
 # Start of element R1
 ChangePinName(R1, 1, 1)
 ChangePinName(R1, 3, 3)
 ChangePinName(R1, 2, 2)
 
-# Start of element C3
-ChangePinName(C3, 2, 2)
-ChangePinName(C3, 1, 1)
-
 # Start of element R2
 ChangePinName(R2, 1, 1)
 ChangePinName(R2, 2, 2)
-
-# Start of element J1
-ChangePinName(J1, 3, 3)
-ChangePinName(J1, 2, 2)
-ChangePinName(J1, 1, 1)
-
-# Start of element C2
-ChangePinName(C2, 2, 2)
-ChangePinName(C2, 1, 1)
-
-# Start of element C1
-ChangePinName(C1, 2, 2)
-ChangePinName(C1, 1, 1)
-
-# Start of element J2
-ChangePinName(J2, 5, GND)
-ChangePinName(J2, 3, D+)
-ChangePinName(J2, 2, D-)
-ChangePinName(J2, 1, Vbus)
-
-# Start of element U2
-ChangePinName(U2, 4, C2+)
-ChangePinName(U2, 3, C2-)
-ChangePinName(U2, 2, C1+)
-ChangePinName(U2, 1, C1-)
-ChangePinName(U2, 5, Vout)
-ChangePinName(U2, 6, Vcc)
-ChangePinName(U2, 7, GND)
-ChangePinName(U2, 8, SHDN)
 
 # Start of element U1
 ChangePinName(U1, 1, V+)
